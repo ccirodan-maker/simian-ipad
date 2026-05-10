063631f144fc195e37aad0240b35c15cc5d1ad63
ccirodan-maker <ccirodan@gmail.com>
2026-05-10 16:26:40 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 //  バージョンを更新したら CACHE_VERSION を上げる。古いキャッシュは
 //  activate イベントで掃除される。
 // =============================================================
-const CACHE_VERSION = 'simant-ipad-v1';
+const CACHE_VERSION = 'simant-ipad-v2';
 const ASSETS = [
   './',
   './index.html',
